f40b3c7689dbb8fdb6fcf153812492ddb85b20a5
danillopneto <daynilou@gmail.com>
2020-12-31 17:09:29 -0300

    Adding swagger documentation

--- mypocket-api/Startup.cs
+++ mypocket-api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using mypocket.api.Mapping;
 
 namespace mypocket.api
@@ -22,6 +23,11 @@ namespace mypocket.api
             services.AddControllers();
 
             services.AddScoped<IProductMapping, ProductMapping>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Pocket", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +47,14 @@ namespace mypocket.api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
             });
         }
     }
